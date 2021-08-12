fc8f382ee87ebca10b95aa2986b028b6c3a54aa8
Stackbit Code Editor <projects@stackbit.com>
2021-08-12 04:04:11 +0000

    index.js: changed

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,6 +16,53 @@ const IndexPage = () => (
           </p>
         </Col>
       </Row>
+      <Row className="justify-content-center my-3">
+        <Col md="6">
+          <ListGroup>
+            <ListGroup.Item
+              action
+              href="https://getbootstrap.com"
+              target="_blank"
+            >
+              Bootstrap
+            </ListGroup.Item>
+            <ListGroup.Item
+              action
+              href="https://react-bootstrap.github.io/"
+              target="_blank"
+            >
+              react-bootstrap
+            </ListGroup.Item>
+            <ListGroup.Item
+              action
+              href="https://react-icons.netlify.com"
+              target="_blank"
+            >
+              react-icons
+            </ListGroup.Item>
+            <ListGroup.Item
+              action
+              href="https://www.gatsbyjs.org/packages/gatsby-plugin-sass/"
+              target="_blank"
+            >
+              gatsby-plugin-sass
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <p>
+            This starter also includes a navbar that sticks to the top of the
+            screen when the user scrolls past it, and a footer that stays at the
+            bottom of the screen.
+          </p>
+          <p>
+            For more documentation on these packages and how they work, please
+            refer to the pages linked in the list above.
+          </p>
+        </Col>
+      </Row>
     </Container>
   </Layout>
 )
